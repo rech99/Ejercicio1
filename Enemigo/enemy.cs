47d0aac175071c5c69f3551e44b6e58406be0975
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-15 07:39:36 -0700

    Agregar archivos de proyecto.

---
+++ Enemigo/enemy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enemigo
+{
+    class enemy
+    {
+
+        public int posx;
+        public int posy;
+        private int _score;
+        public int score
+            
+        {
+            get
+            {
+                return _score;
+            }
+
+            set
+            {
+                if (value >= 100)
+                {
+                    _score = value;
+                }
+
+                else
+                {
+                    throw new Exception("No hay score suficiente");
+                }
+
+            }
+        }
+        
+        
+         
+        
+        
+    }
+
+
+
+
+
+    
+}
+}
